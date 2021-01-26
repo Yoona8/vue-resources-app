9cc03ae578ae5e5a4824ee2e78caaf112038734c
Yoona8 <elena243v@gmail.com>
2021-01-26 12:28:07 +0300

    Add props

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <template>
   {{ title }}
+  <ul>
+    <resource-item
+      v-for="resource in resources"
+      :key="resource.id"
+      :name="resource.name"
+    ></resource-item>
+  </ul>
 </template>
 
 <script>
+import ResourceItem from '@/components/ResourceItem';
 export default {
+  components: {ResourceItem},
   data() {
     return {
-      title: 'Hello!'
+      title: 'Hello!',
+      resources: [{
+        id: Date.now() + Math.random(),
+        name: 'Learn JS',
+        url: '',
+        tag: 'javascript',
+        isFinished: true
+      }, {
+        id: Date.now() + Math.random(),
+        name: 'Learn CSS animations',
+        url: '',
+        tag: 'css',
+        isFinished: false
+      }]
     };
   }
 };
---
+++ src/components/ResourceItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <li>{{ name }}</li>
+</template>
+
+<script>
+export default {
+  name: 'resource-item',
+  props: {
+    name: {
+      type: String,
+      required: true,
+      validator: (value) => {
+        return value.length > 2;
+      }
+    }
+  },
+  data() {
+    return {
+      resourceName: 'Some resource'
+    };
+  }
+}
+</script>
+
+<style></style>
